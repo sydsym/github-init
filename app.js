94035da6e2ac5153047800cad00d75ab67d75887
sydsym <sayeedsym@gmail.com>
2024-04-28 08:13:21 -0400

    just fun staff

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const age = 10;
+// const age = 10;
 
-age >= 18 ? console.log("vote dio") : console.log("no vote for u ");
+// age >= 18 ? console.log("vote dio") : console.log("no vote for u ");
 
-const isLeader = false;
+// const isLeader = false;
 
-isLeader ? console.log("Salam") : console.log("ja vag shala");
+// isLeader ? console.log("Salam") : console.log("ja vag shala");
+
+// for( let i = 1; i <= 100; i++){
+//     if(i % 2 != 1){
+//         console.log(i);
+//     }
+// }
+
+function square(number){
+    const square = number * number;
+    console.log(square);
+}
+
+square(5);
